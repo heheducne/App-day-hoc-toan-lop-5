98f7bf17dcf8dbc6c52f3468b786bf85c119bf98
ducdanglambai <phuocducbui9@gmail.com>
2023-01-08 19:27:58 +0700

    day2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_main));
             this.panel_ho_va_ten = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.main_comboBox_ho_va_ten = new System.Windows.Forms.ComboBox();
             this.main_label_lop = new System.Windows.Forms.Label();
             this.main_label_truong = new System.Windows.Forms.Label();
-            this.main_comboBox_ho_va_ten = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.main_pictureBox_luyentap = new System.Windows.Forms.PictureBox();
             this.main_pictureBox_lythuyet = new System.Windows.Forms.PictureBox();
@@ -45,16 +46,15 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.main_pictureBox_info = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.main_panel_taohocsinhmoi = new System.Windows.Forms.Panel();
+            this.main_textBox_lop = new System.Windows.Forms.TextBox();
+            this.main_textBox_truong = new System.Windows.Forms.TextBox();
+            this.main_textBox_ten = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.main_button_taohocsinhmoi = new System.Windows.Forms.Button();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
-            this.main_textBox_ten = new System.Windows.Forms.TextBox();
-            this.main_textBox_truong = new System.Windows.Forms.TextBox();
-            this.main_textBox_lop = new System.Windows.Forms.TextBox();
             this.panel_ho_va_ten.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.main_pictureBox_luyentap)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.main_pictureBox_lythuyet)).BeginInit();
@@ -81,38 +81,37 @@
             this.panel_ho_va_ten.Size = new System.Drawing.Size(385, 236);
             this.panel_ho_va_ten.TabIndex = 0;
             // 
-            // label1
+            // label4
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(11, 55);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 22);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Học sinh:";
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(92, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 22);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Thông tin học sinh";
             // 
-            // label2
+            // button2
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(11, 103);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 22);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Trường:";
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(205, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Chưa có thông tin ?";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label3
+            // main_comboBox_ho_va_ten
             // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(11, 147);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 22);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Lớp:";
+            this.main_comboBox_ho_va_ten.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.main_comboBox_ho_va_ten.FormattingEnabled = true;
+            this.main_comboBox_ho_va_ten.Location = new System.Drawing.Point(123, 57);
+            this.main_comboBox_ho_va_ten.Name = "main_comboBox_ho_va_ten";
+            this.main_comboBox_ho_va_ten.Size = new System.Drawing.Size(169, 24);
+            this.main_comboBox_ho_va_ten.TabIndex = 6;
             // 
             // main_label_lop
             // 
@@ -136,26 +135,38 @@
             this.main_label_truong.TabIndex = 4;
             this.main_label_truong.Text = "trường";
             // 
-            // main_comboBox_ho_va_ten
+            // label3
             // 
-            this.main_comboBox_ho_va_ten.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.main_comboBox_ho_va_ten.FormattingEnabled = true;
-            this.main_comboBox_ho_va_ten.Location = new System.Drawing.Point(123, 57);
-            this.main_comboBox_ho_va_ten.Name = "main_comboBox_ho_va_ten";
-            this.main_comboBox_ho_va_ten.Size = new System.Drawing.Size(169, 24);
-            this.main_comboBox_ho_va_ten.TabIndex = 6;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lớp:";
             // 
-            // button2
+            // label2
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(205, 194);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(165, 37);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Chưa có thông tin ?";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Trường:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Học sinh:";
             // 
             // label6
             // 
@@ -212,7 +223,7 @@
             this.label7.ForeColor = System.Drawing.Color.Yellow;
             this.label7.Location = new System.Drawing.Point(494, 435);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(272, 52);
+            this.label7.Size = new System.Drawing.Size(267, 51);
             this.label7.TabIndex = 5;
             this.label7.Text = "LÝ THUYẾT";
             // 
@@ -224,7 +235,7 @@
             this.label8.ForeColor = System.Drawing.Color.Yellow;
             this.label8.Location = new System.Drawing.Point(97, 435);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(274, 52);
+            this.label8.Size = new System.Drawing.Size(270, 51);
             this.label8.TabIndex = 6;
             this.label8.Text = "LUYỆN TẬP";
             // 
@@ -236,7 +247,7 @@
             this.label9.ForeColor = System.Drawing.Color.Yellow;
             this.label9.Location = new System.Drawing.Point(879, 435);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(237, 52);
+            this.label9.Size = new System.Drawing.Size(233, 51);
             this.label9.TabIndex = 7;
             this.label9.Text = "KIỂM TRA";
             // 
@@ -245,22 +256,11 @@
             this.main_pictureBox_info.BackColor = System.Drawing.Color.Transparent;
             this.main_pictureBox_info.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("main_pictureBox_info.BackgroundImage")));
             this.main_pictureBox_info.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.main_pictureBox_info.Location = new System.Drawing.Point(1149, 0);
+            this.main_pictureBox_info.Location = new System.Drawing.Point(1157, 0);
             this.main_pictureBox_info.Name = "main_pictureBox_info";
             this.main_pictureBox_info.Size = new System.Drawing.Size(100, 100);
             this.main_pictureBox_info.TabIndex = 8;
             this.main_pictureBox_info.TabStop = false;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(92, 17);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(176, 22);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Thông tin học sinh";
             // 
             // main_panel_taohocsinhmoi
             // 
@@ -280,6 +280,27 @@
             this.main_panel_taohocsinhmoi.Size = new System.Drawing.Size(385, 236);
             this.main_panel_taohocsinhmoi.TabIndex = 10;
             this.main_panel_taohocsinhmoi.Visible = false;
+            // 
+            // main_textBox_lop
+            // 
+            this.main_textBox_lop.Location = new System.Drawing.Point(130, 149);
+            this.main_textBox_lop.Name = "main_textBox_lop";
+            this.main_textBox_lop.Size = new System.Drawing.Size(184, 22);
+            this.main_textBox_lop.TabIndex = 12;
+            // 
+            // main_textBox_truong
+            // 
+            this.main_textBox_truong.Location = new System.Drawing.Point(130, 105);
+            this.main_textBox_truong.Name = "main_textBox_truong";
+            this.main_textBox_truong.Size = new System.Drawing.Size(184, 22);
+            this.main_textBox_truong.TabIndex = 11;
+            // 
+            // main_textBox_ten
+            // 
+            this.main_textBox_ten.Location = new System.Drawing.Point(130, 57);
+            this.main_textBox_ten.Name = "main_textBox_ten";
+            this.main_textBox_ten.Size = new System.Drawing.Size(184, 22);
+            this.main_textBox_ten.TabIndex = 10;
             // 
             // label5
             // 
@@ -336,27 +357,6 @@
             this.label14.Size = new System.Drawing.Size(94, 22);
             this.label14.TabIndex = 0;
             this.label14.Text = "Học sinh:";
-            // 
-            // main_textBox_ten
-            // 
-            this.main_textBox_ten.Location = new System.Drawing.Point(130, 57);
-            this.main_textBox_ten.Name = "main_textBox_ten";
-            this.main_textBox_ten.Size = new System.Drawing.Size(184, 22);
-            this.main_textBox_ten.TabIndex = 10;
-            // 
-            // main_textBox_truong
-            // 
-            this.main_textBox_truong.Location = new System.Drawing.Point(130, 105);
-            this.main_textBox_truong.Name = "main_textBox_truong";
-            this.main_textBox_truong.Size = new System.Drawing.Size(184, 22);
-            this.main_textBox_truong.TabIndex = 11;
-            // 
-            // main_textBox_lop
-            // 
-            this.main_textBox_lop.Location = new System.Drawing.Point(130, 149);
-            this.main_textBox_lop.Name = "main_textBox_lop";
-            this.main_textBox_lop.Size = new System.Drawing.Size(184, 22);
-            this.main_textBox_lop.TabIndex = 12;
             // 
             // Form_main
             // 
--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -30,11 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_lythuyet));
             this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label6
@@ -49,66 +49,34 @@
             this.label6.TabIndex = 2;
             this.label6.Text = "LÝ THUYẾT";
             // 
-            // button1
+            // pictureBox1
             // 
-            this.button1.BackColor = System.Drawing.Color.Yellow;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(25, 187);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(235, 227);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "CHƯƠNG 1: \r\n\r\n\r\nÔn tập và bổ sung về phân số.\r\nGiải toán liên quan đến tỉ lệ.\r\nBả" +
-    "ng đơn vị đo diện tích\r\n";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button1.UseVisualStyleBackColor = false;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(1161, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
             // 
-            // button2
+            // label1
             // 
-            this.button2.BackColor = System.Drawing.Color.Yellow;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(266, 346);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(235, 227);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "CHƯƠNG 2: \r\n\r\n\r\nSố thập phân.\r\nCác phép tính với số thập phân";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button2.UseVisualStyleBackColor = false;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TOÁN LỚP 5";
             // 
-            // button3
+            // panel1
             // 
-            this.button3.BackColor = System.Drawing.Color.Yellow;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(507, 187);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(235, 227);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "CHƯƠNG 3: \r\n\r\n\r\nHình học";
-            this.button3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button3.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.Yellow;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(748, 346);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(235, 227);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "CHƯƠNG 4:\r\n\r\n\r\nSố đo thời gian.\r\nToán chuyển động đều";
-            this.button4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button4.UseVisualStyleBackColor = false;
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.Color.Yellow;
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(989, 187);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(235, 227);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "CHƯƠNG 5:\r\n\r\n\r\nÔn tập cuối năm";
-            this.button5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button5.UseVisualStyleBackColor = false;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 680);
+            this.panel1.TabIndex = 9;
             // 
             // Form_lythuyet
             // 
@@ -117,15 +85,15 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1257, 678);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label6);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form_lythuyet";
             this.Text = "Lý thuyết";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,10 +102,8 @@
         #endregion
 
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
