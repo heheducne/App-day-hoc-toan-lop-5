60100e35a4b253081b56b6da04229aadefda75a3
ducdanglambai <phuocducbui9@gmail.com>
2023-01-09 14:51:49 +0700

    day3

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -35,10 +35,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1257, 678);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1582, 753);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1600, 800);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1600, 800);
             this.Name = "Form_luyentap";
-            this.Text = "Luyện tập";
+            this.Text = "Thi thử 45 phút";
             this.ResumeLayout(false);
 
         }
--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -29,20 +29,254 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_kiemtra));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.lythuyet_pictureBox_back = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lythuyet_pictureBox_back)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepPink;
+            this.label1.Location = new System.Drawing.Point(201, 610);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1103, 113);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KIỂM TRA KIẾN THỨC";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(199, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 171);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "TRẮC NGHIỆM\r\n\r\n10 câu hỏi - 15 phút";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DeepPink;
+            this.label2.Location = new System.Drawing.Point(190, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(421, 52);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CHỌN HÌNH THỨC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DeepPink;
+            this.label3.Location = new System.Drawing.Point(1014, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(344, 52);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "CHỌN CHỦ ĐỀ";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(434, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 171);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "TỰ LUẬN\r\n\r\n10 câu hỏi - 15 phút";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Green;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Yellow;
+            this.button3.Location = new System.Drawing.Point(845, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 105);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Chương 1";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Green;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Yellow;
+            this.button4.Location = new System.Drawing.Point(1026, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 105);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Chương 2";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Green;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Yellow;
+            this.button5.Location = new System.Drawing.Point(1201, 121);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(157, 105);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Chương 3";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Green;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Yellow;
+            this.button6.Location = new System.Drawing.Point(1201, 254);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(157, 105);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Tất cả";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Green;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.Yellow;
+            this.button7.Location = new System.Drawing.Point(1026, 254);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(157, 105);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Chương 5";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Green;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.Yellow;
+            this.button8.Location = new System.Drawing.Point(845, 254);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(157, 105);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Chương 4";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 754);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(1402, -4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(186, 754);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.Transparent;
+            this.button9.Location = new System.Drawing.Point(589, 412);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(410, 73);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "BẮT ĐẦU LÀM BÀI";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // lythuyet_pictureBox_back
+            // 
+            this.lythuyet_pictureBox_back.BackColor = System.Drawing.Color.Transparent;
+            this.lythuyet_pictureBox_back.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("lythuyet_pictureBox_back.BackgroundImage")));
+            this.lythuyet_pictureBox_back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.lythuyet_pictureBox_back.Location = new System.Drawing.Point(727, -4);
+            this.lythuyet_pictureBox_back.Name = "lythuyet_pictureBox_back";
+            this.lythuyet_pictureBox_back.Size = new System.Drawing.Size(100, 100);
+            this.lythuyet_pictureBox_back.TabIndex = 14;
+            this.lythuyet_pictureBox_back.TabStop = false;
+            this.lythuyet_pictureBox_back.Click += new System.EventHandler(this.lythuyet_pictureBox_back_Click);
             // 
             // Form_kiemtra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1257, 678);
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1582, 753);
+            this.Controls.Add(this.lythuyet_pictureBox_back);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1600, 800);
+            this.MinimumSize = new System.Drawing.Size(1600, 800);
             this.Name = "Form_kiemtra";
-            this.Text = "Kiểm tra";
+            this.Text = "Kiểm tra kiến thức";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lythuyet_pictureBox_back)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.PictureBox lythuyet_pictureBox_back;
     }
 }
